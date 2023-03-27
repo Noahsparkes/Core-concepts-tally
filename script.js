1783539c580af827061dd5628b6a5335ae0a18a9
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-03-27 18:04:43 +0200

    changes to functions in JS

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 
+console.log("123" + 4 ,parseInt(123)+ 4)
 
 const subtractHandler = () => {
-number.value = number.value - 1;
+    const newValue = parseInt( number.value) - 1
+    number.value = newValue
 }
 
 const addHandler = () => {
-    number.value = number.value + 1;
+    const newValue = parseInt( number.value) +1
+    number.value = newValue
 }
+
 
 subtract.addEventListener('click', subtractHandler)
 add.addEventListener('click', addHandler)
