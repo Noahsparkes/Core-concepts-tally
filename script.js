ba8ca22a7ed6b955692bafebb99c5ec5f5ba173a
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-03-27 19:48:38 +0200

    changes to if statements in JS

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const MAX_NUMBER = 10
+const MIN_NUMBER = -5
+
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
@@ -6,13 +9,29 @@ const add = document.querySelector('[data-key="add"]')
 const subtractHandler = () => {
     const newValue = parseInt( number.value) - 1
     number.value = newValue
+
+    if (number.value.disabled === true) {
+        add.disabled = false
+    }
+    
+
+   if (newValue <= MIN_NUMBER) {
+     subtract.disabled = true
+    }
 }
 
 const addHandler = () => {
     const newValue = parseInt( number.value) +1
     number.value = newValue
-}
 
+    if () {
+        add.disabled = false   
+    }
+
+    if (newValue >= MAX_NUMBER) {
+        add.disabled = true
+    }
+}
 
 subtract.addEventListener('click', subtractHandler)
 add.addEventListener('click', addHandler)
